1538ca4cf37abd14f45cee2db7a69658d74c390e
obada <awaisali3405@gmail.com>
2023-12-12 20:28:58 +0500

    coomit

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -2,17 +2,17 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8"/>
+    <meta charset="utf-8" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @if(!empty($metas))
-        @if($metas['meta_description'])
-            <meta name="description" content="{{$metas['meta_description']}}">
+    @if (!empty($metas))
+        @if ($metas['meta_description'])
+            <meta name="description" content="{{ $metas['meta_description'] }}">
         @endif
-        @if($metas['meta_keyword'])
-            <meta name="keywords" content="{{$metas['meta_keyword']}}">
+        @if ($metas['meta_keyword'])
+            <meta name="keywords" content="{{ $metas['meta_keyword'] }}">
         @endif
-        @if($metas['home_title'] && $metas['site_title'])
+        @if ($metas['home_title'] && $metas['site_title'])
             <title>{{ $metas['home_title'] }} | {{ $metas['site_title'] }}</title>
         @else
             <title>@yield('title') | {{ getAppName() }}</title>
@@ -23,23 +23,23 @@
         <meta name="keywords" content="">
     @endif
 
-    @if(!empty(getAppLogo()))
-        <meta property="og:image" content="{{getAppLogo()}}"/>
+    @if (!empty(getAppLogo()))
+        <meta property="og:image" content="{{ getAppLogo() }}" />
     @endif
 
     <link rel="icon" href="{{ getFaviconUrl() }}" type="image/png">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-          crossorigin="anonymous" referrerpolicy="no-referrer"/>
-    <link href="{{ mix('assets/css/public.css') }}" rel="stylesheet" type="text/css">
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="{{ asset('assets/css/public.css') }}" rel="stylesheet" type="text/css">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/third-party.css') }}">
     <link href="{{ asset('assets/css/front-custom.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('assets/css/front/front-custom.css') }}" rel="stylesheet" type="text/css">
     <script src="{{ mix('assets/js/front-third-party.js') }}"></script>
     <script src="{{ asset('assets/js/messages.js') }}"></script>
-    @php  $langSession = Session::get('languageName');
-           $frontLanguage = !isset($langSession) ? getSuperAdminSettingValue('default_language') : $langSession;
+    @php$langSession = Session::get('languageName');
+        $frontLanguage = !isset($langSession) ? getSuperAdminSettingValue('default_language') : $langSession;
     @endphp
     <script>
         let frontLanguage = "{{ $frontLanguage }}"
@@ -49,15 +49,15 @@
 
     {!! getSuperAdminSettingValue('extra_js_front') !!}
     @routes
-    {{--@yield('page_js')--}}
-    {{--@yield('scripts')--}}
+    {{-- @yield('page_js') --}}
+    {{-- @yield('scripts') --}}
     <script>
         $('html, body').animate({
             scrollTop: $('html, body').offset().top,
         })
     </script>
     <!--google analytics code-->
-    @if(!empty($metas['google_analytics']))
+    @if (!empty($metas['google_analytics']))
         <script>
             {!! $metas['google_analytics'] !!}
         </script>
@@ -65,9 +65,10 @@
 </head>
 
 <body data-bs-offset="71">
-<!-- start header section -->
-@include('front.layouts.header')
-@yield('content')
-@include('front.layouts.footer')
+    <!-- start header section -->
+    @include('front.layouts.header')
+    @yield('content')
+    @include('front.layouts.footer')
 </body>
+
 </html>
